3dd5717286a4dc6114b7144c2630f5bde9c3c1a9
Isaac Saenz <isaacsaenz387@gmail.com>
2026-05-01 21:28:28 -0600

    se añadio el problema 1022 de beecrowd judge

---
+++ data-structure/1022.py
@@ -0,0 +1,27 @@
+import math
+
+casos_prueba = int(input())
+
+for _ in range(casos_prueba):
+    
+    n1, _, d1, op, n2, _, d2 = input().split()
+    n1, d1, n2, d2 = int(n1), int(d1), int(n2), int(d2)
+
+    if op == "+":
+        numerador = n1 * d2 + n2 * d1
+        denominador = d1 * d2
+    elif op == "-":
+        numerador = n1*d2 - n2*d1
+        denominador = d1 * d2
+    elif op == "*":
+        numerador = n1*n2 
+        denominador = d1 * d2
+    elif op == "/":
+        numerador = n1 * d2
+        denominador = n2 * d1
+        
+    mcd = math.gcd(numerador, denominador)
+
+    numerador_simplificado = numerador // mcd
+    denominador_simplificado = denominador // mcd
+    print(f"{numerador} / {denominador} = {numerador_simplificado} / {denominador_simplificado}")
